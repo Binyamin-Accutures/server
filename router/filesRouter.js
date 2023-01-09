4f081d395c04f069e69e14719208d2622d30281d
amit glaicher <amitglaicher98@gmail.com>
2023-01-09 12:42:24 +0200

    check

--- router/filesRouter.js
+++ router/filesRouter.js
@@ -6,28 +6,55 @@ const upload = multer({dest:"./uploads"})
 const fs = require('fs');
 const userData = require('../DL/user.model');
 const userService = require('../BL/user.service');
+const { errController } = require('../errController');
+errController = require('../errController')
 
 filesRouter.use('/',express.static('uploads'))
 
 
-filesRouter.get('/', async (req,res)=>{
-// const files = await userService.
-
-    const response = fs.readdirSync(req.params.path)
-    const files = response.map(v=>{
-        if(fs.lstatSync(v).isDirectory()){
+filesRouter.get('/', async (req,res, next)=>{
+    try{
+        const filesPath = await userService.getFiles(req.send.email)
+        const files = filesPath.map((v)=>{
+            const file = v.root.replace("upload/","").split(".")
+            return {
+                date:file.pop(),
+                name:file.join("."),
                 
+            }})
+            res.send(files)
         }
-    })
-}) 
+        catch(err){
+            req.errCode = err.code
+            next()
+        }
+},errController())
 
-filesRouter.post('/', upload.array("files"), async (req,res)=>{
 
-})
+filesRouter.post('/', upload.array("files"), async (req,res, next)=>{
+try{
+
+}
+
+catch(err){
+
+}
 
 
-filesRouter.put('/:filename',auth.verifyToken,(req, res)=>{
-})
+},errController())
+
+
+filesRouter.put('/:filename',auth.verifyToken,(req, res, next)=>{
+    try{
+
+    }
+    
+    catch(err){
+    
+    }
+    
+    
+    },errController())
 
 module.exports = filesRouter
 
