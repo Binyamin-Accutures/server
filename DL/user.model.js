3cc15a9926fe593afea12dca1077f7e4ca09d73a
Shira Grossman <shiradeshen4@gmail.com>
2023-01-09 16:53:38 +0200

    fixing

--- DL/user.model.js
+++ DL/user.model.js
@@ -1,15 +1,6 @@
-require("./db").connect();
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  fName: {
-    type: String,
-    required: true,
-  },
-  lName: {
-    type: String,
-    required: true,
-  },
   email: {
     type: String,
     unique: true,
