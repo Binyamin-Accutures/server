ea9919df696dd5e1255284891e7b793a7d28fda6
amit glaicher <amitglaicher98@gmail.com>
2023-01-10 14:27:19 +0200

    before merge with david

--- BL/user.service.js
+++ BL/user.service.js
@@ -1,34 +1,22 @@
 const userDL =require( '../DL/user.controller')
 const auth =  require('../auth')
 const bcrypt = require('bcrypt')
+const {ChechData} = require('../checkController')
 
 const saltRounds = 10
 
 const login = async (data) => {
-    if (!data.email || !data.password){
-        throw {code : 400, message : "missing data"}
-    }
-    
+    ChechData(data,['email', 'password'])
     let user = await getUser(data.email)
-    if (!user){
-        throw {code: 400, message : "no user found"}
-    }
     const bcrypted =bcrypt.compareSync( data.password,user.password)
-    if (!bcrypted){
-        throw {code: 400, message : "worng password is incorrect"}
-    }
+    if (!bcrypted) throw errMessage.WRONG_PASSWORD
     let token = await auth.createToken(data.email)
     return token
 }
 
 const createUser = async (data) => {
+    ChechData(data,['email', 'firstPassword', 'secondPassword']);
     let user = await getUser( data.email)
-    if (user){
-        throw {code : 400, message : "user exists"}
-    }
-    if (!data.email || !data.password){
-        throw {code : 400, message : "missing data"}
-    }
     data.password = bcrypt.hashSync(data.password, saltRounds);
     user = await userDL.create(data)
     let token = await auth.createToken(data.email)
@@ -36,15 +24,14 @@ const createUser = async (data) => {
 }
 
 const getUser = async (email) => {
-    const check =await userDL.readOne({email : email})
-    return check
+    const user =await userDL.readOne({email : email})
+    if (!user) throw errMessage.USER_NOT_FOUND;
+    return user
 }
 
 const getFiles = async (email) => {
     let user = await getUser(email)
-    if (!user){
-        throw {code: 400, message : "no user found"}
-    }
+    if (!user) throw errMessage.USER_NOT_FOUND;
     return user.projects
 } 
 
