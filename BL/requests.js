8d6603584ee55b739b3e30aeca979180eec2e19c
Yehoshua Tarshish <a0526741852@gmail.com>
2023-01-11 10:16:22 +0200

    changes

--- BL/requests.js
+++ BL/requests.js
@@ -2,18 +2,22 @@ const {userCtrl}=require ("../DL/user.controller")
 const projectsCtrl = require ("../DL/project.controller") 
 const userService = require ("./") 
 
-const saveIspObj = async (email, obj)=>{
-    const user = await userService.getUser(req.email)
+const saveIspObj = async (root,email, obj)=>{
+    const user = await userService.getUser(email)
     if (user){
-const sentIspObj = await createProject(user.)
+const sentIspObj = await projectsCtrl.updateProject({root:root, saveSettings:{runIspSettings:obj}})
 
     }
-
+    (user._id,{ root:`./${baseDir}`,runIspSettings: {undefined },createDate: date})
 }
+const getFile = async (root) => {
+    const project = await projDL.readOne({root})
+    return project
+} 
 
-const createProject = async (user_id, data) =>{
-    checkData({user_id,...data},["root", "runIspSettings"])
-    const newProject = await projDL.create(data)
-    const res = await userService.addProject(user_id, newProject)
-    return errMessage.SUCCESS
+const updateProject = async (root,saveSettings) =>{//no error return. //
+    checkData({root:root,saveSettings:saveSettings},["root","saveSettings"])
+    const project = await projDL.readOne({root})
+    const res = await projDL.updateAndReturn(project._id, {saveSettings})
+    return res
 }
