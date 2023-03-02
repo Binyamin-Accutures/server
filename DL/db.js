e959dcd7522135ec36970acc3e0da105f44e0383
matanelVatkin <matanelVatkin@gmail.com>
2023-03-02 09:42:51 +0200

    add install labry function

--- DL/db.js
+++ DL/db.js
@@ -1,15 +1,28 @@
-const mongoose = require('mongoose');
+const { exec } = require("child_process");
+const mongoose = require("mongoose");
 const MONGO_URL = process.env.MONGO_URL;
 
-mongoose.set('strictQuery', false);
+mongoose.set("strictQuery", false);
 
 async function connect() {
   try {
+    const packages = ["matplotlib", "numpy", "sys","scikit-image","opencv-python"];
+    for (package in packages){
+      exec(`pip install ${package}`, (error, stdout, stderr) => {
+        if (error) {
+          console.error(
+            `Failed to install package ${package}. Error message: ${error}`
+          );
+          return;
+        }
+        console.log(`Package ${package} successfully installed.`);
+      });
+    }
     mongoose.connect(
       MONGO_URL,
       { useNewUrlParser: true, useUnifiedTopology: true },
       (err) => {
-        if (err) throw 'Error DB : ' + err;
+        if (err) throw "Error DB : " + err;
 
         console.log(`Connection Success`);
       }
@@ -21,3 +34,7 @@ async function connect() {
 }
 
 module.exports = { connect };
+
+const { exec } = require("child_process");
+
+// Specify the package name
